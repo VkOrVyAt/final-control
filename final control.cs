1a995be0ec58fb3f4c94a0929b6824a3c0a99137
Valdimir Isakov <vladimir.isakov.0101@mail.ru>
2024-01-15 13:58:30 +0500

    final version

--- final control.cs
+++ final control.cs
@@ -13,7 +13,7 @@ namespace Solution
             foreach (string item in list)
                 Console.Write(item + " ");
         }
-        
+
         public static List<string> SortArray(string[] array)
         {
             var result = new List<string>();
@@ -28,6 +28,9 @@ namespace Solution
             var test1 = new string[4] {"rere", "ree", "r", "re"};
             var test2 = new string[3] {"", "e", "."};
             var test3 = new string[0];
+            PrintList(SortArray(test1));
+            PrintList(SortArray(test2));
+            PrintList(SortArray(test3));
         }
     }
 }
