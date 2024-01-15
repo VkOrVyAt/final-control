00e264a7219055b8cef78baa3abf796ce60bc5b0
Valdimir Isakov <vladimir.isakov.0101@mail.ru>
2024-01-15 13:56:52 +0500

    Write function for sort array

--- final control.cs
+++ final control.cs
@@ -8,6 +8,15 @@ namespace Solution
 {
     public class Programm 
     {
+        public static List<string> SortArray(string[] array)
+        {
+            var result = new List<string>();
+            foreach (var item in array)
+                if (item.Length <= 3)
+                    result.Add(item);
+            return result;
+        }
+
         public void Main()
         {
             var test1 = new string[4] {"rere", "ree", "r", "re"};
